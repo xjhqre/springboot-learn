faca9299b15642c113872ad9e52be3fcb38cafe9
xjhqre <859425698@qq.com>
2023-09-13 15:12:44 +0800

    update

---
+++ springboot-idempotence/src/main/java/com/xjhqre/IdempotenceApp.java
@@ -0,0 +1,19 @@
+package com.xjhqre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * IdempotenceApp
+ * <p>
+ *
+ * @author xjhqre
+ * @since 9月 13, 2023
+ */
+@SpringBootApplication
+public class IdempotenceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(IdempotenceApp.class, args);
+    }
+}
